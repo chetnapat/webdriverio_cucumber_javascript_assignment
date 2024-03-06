65e9fa7e2279239e16e45eafe8033027a6272c4d
Chetana Patil <chetana.patil@clariontech.com>
2024-03-06 10:36:28 +0530

    Made changes

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -2,8 +2,8 @@ const { Given, When, Then } = require('@wdio/cucumber-framework');
 const { expect, $ } = require('@wdio/globals')
 const LoginPage = require('../pageobjects/login.page');
 const productsPage = require('../pageobjects/products.page');
-const cartPage = require('../pageobjects/cart.page');
-const checkoutPage = require('../pageobjects/checkout.page');
+const YourCartPage = require('../pageobjects/cart.page');
+const CheckoutPage = require('../pageobjects/checkout.page');
 
 Given(/^I am on the (\w+) page$/, async (page) => {
     await LoginPage.open()
@@ -20,7 +20,7 @@ Then(/^user should navigate to products page$/, async () => {
 });
 
 Then(/^user should see error <message>$/, async(message) => {
-    const errorMessage = await loginPage.getLoginErrorMsg();
+    const errorMessage = await LoginPage.getLoginErrorMsg();
     console.log('Actual error message for invalid credentials is : ' + errorMessage);
     expect(errorMessage).toContain(message);
 });
@@ -37,29 +37,29 @@ When(/^user clicks on Add to cart button$/, async() => {
 
 When(/^user navigates to cart$/, async() => {
     await productsPage.clickOnShoppingCartIcon();
-    const cartPageHeader = await cartPage.getYourCartPageHeader();
+    const cartPageHeader = await YourCartPage.getYourCartPageHeader();
     expect(cartPageHeader).toEqual('Your Cart');
 });
 
 Then(/^user should see same product on your cart page$/, async() => {
-    const productNameOnCartPage = await cartPage.getProductNameOnYourCartPage();
+    const productNameOnCartPage = await YourCartPage.getProductNameOnYourCartPage();
     console.log("Product Name On Cart Page is : " + productNameOnCartPage);
     expect(productNameOnCartPage).toEqual(firstProductNameOnProductsPage);
 });
 
 
 When(/^user clicks on checkout button$/, async() => {
-	await cartPage.clickOnCheckoutBtn();
+	await YourCartPage.clickOnCheckoutBtn();
 });
 
 When(/^user enters <firstName>, <lastName> and <postalCode>$/, async(firstName, lastname, postalCode) => {
-    await checkoutPage.enterDetails(firstName, lastname, postalCode);
-    await checkoutPage.clickOnContinueBtn();
-    await checkoutPage.clickOnFinishBtn();
+    await CheckoutPage.enterDetails(firstName, lastname, postalCode);
+    await CheckoutPage.clickOnContinueBtn();
+    await CheckoutPage.clickOnFinishBtn();
 });
 
 Then(/^user should see success <message>$/, async(message) => {
-    const successMessageOnoOrderPlcaed = await checkoutPage.getSuccessMessage();
+    const successMessageOnoOrderPlcaed = await CheckoutPage.getSuccessMessage();
     console.log("Success message after placing order is : " + successMessageOnoOrderPlcaed);
     expect(successMessageOnoOrderPlcaed).toEqual(message);
 });
